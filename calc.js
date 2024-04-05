99ddc6da7ad0db9a6bd0d6d651f67c4329942a00
Fernando <fernandofeques@gmail.com>
2024-04-05 18:04:54 -0300

    Separado em arquivo JS e CSS

---
+++ calc.js
@@ -0,0 +1,44 @@
+function add(){
+  var current, sub;
+  current = f.campoNumero.value;
+  sub = current.charAt(current.length -1);
+  if (sub == '+' || sub == '-' || sub == '*' || sub == '/'){
+    f.campoNumero.value=current.substring(0, current.length-1);
+    f.campoNumero.value+='+';
+  } else {
+    f.campoNumero.value+='+';
+  }
+}
+function minus(){
+  var current, sub;
+  current = f.campoNumero.value;
+  sub = current.charAt(current.length -1);
+  if (sub == '+' || sub == '-' || sub == '*' || sub == '/'){
+    f.campoNumero.value=current.substring(0, current.length-1);
+    f.campoNumero.value+='-';
+  } else {
+    f.campoNumero.value+='-';
+  }
+}
+function multi(){
+  var current, sub;
+  current = f.campoNumero.value;
+  sub = current.charAt(current.length -1);
+  if (sub == '+' || sub == '-' || sub == '*' || sub == '/'){
+    f.campoNumero.value=current.substring(0, current.length-1);
+    f.campoNumero.value+='*';
+  } else {
+    f.campoNumero.value+='*';
+  }
+}
+function divide(){
+  var current, sub;
+  current = f.campoNumero.value;
+  sub = current.charAt(current.length -1);
+  if (sub == '+' || sub == '-' || sub == '*' || sub == '/'){
+    f.campoNumero.value=current.substring(0, current.length-1);
+    f.campoNumero.value+='/';
+  } else {
+    f.campoNumero.value+='/';
+  }
+}
