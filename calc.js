0c3a2576c6799d2efd709a3d92fb6232db44002c
Fernando <fernandofeques@gmail.com>
2024-04-09 20:46:33 -0300

    Implemetando bootstrap

--- calc.js
+++ calc.js
@@ -4,10 +4,8 @@ function add(){
   sub = current.charAt(current.length -1);
   if (sub == '+' || sub == '-' || sub == '*' || sub == '/'){
     f.campoNumero.value=current.substring(0, current.length-1);
-    f.campoNumero.value+='+';
-  } else {
-    f.campoNumero.value+='+';
-  }
+  } 
+  f.campoNumero.value+='+';
 }
 function minus(){
   var current, sub;
